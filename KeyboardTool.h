72e52b23ab8e8add5795f4934c9e2273b6eea5ec
ChoSoodong <zhao543@126.com>
2019-10-21 15:20:39 +0800

    Add files via upload

---
+++ KeyboardTool.h
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KeyboardTool : NSObject
+
+//设置键盘 - Appdelegate里初始化一次
++(void)keyboardSetting;
+
+@end
+
+NS_ASSUME_NONNULL_END
